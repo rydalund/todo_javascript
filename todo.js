30d7c74aa8ed646d05bcb99797b4f582dd5d6e73
Magnus Quist <rydalund@outlook.com>
2020-09-13 01:15:02 +0200

    Trying to get back to coding after summer

--- todo.js
+++ todo.js
@@ -2,51 +2,98 @@ let todoId = 1;
 let todoTitle = "";
 let todoStatus = false;
 let todos = [];
-let showTodo = document.createElement("p");
+let bool = false;
 let btnAddTodo = document.getElementById("btnAddTodo");
 let btnShowTodos = document.getElementById("btnShowTodos");
-let btnDeleteTodo= document.getElementById("btnDeleteTodo");
 
 btnAddTodo.addEventListener("click", submitTodo);
 btnShowTodos.addEventListener("click", showTodos);
-btnDeleteTodo.addEventListener("click", deleteTodo);
 
-function Todo(todoId, todoTitle, todoStatus){
-    this.todoId = todoId++;
-    this.todoTitle = todoTitle;
-    this.todoStatus = todoStatus;
+class Todo {
+    constructor(todoTitle) {
+        this.todoId = todoId++;
+        this.todoTitle = todoTitle;
+        this.todoStatus = todoStatus;
+    }
+  
+    set status(status) {
+        this.todoStatus = status;
+      }
 }
 
-function addTodo(event) {
-    let foo = new Todo(Todo);
+function addTodo(todoTitle) {
+    let foo = new Todo(todoTitle);
     todos.push(foo);
 }
 
 function submitTodo(){
-    let foo = document.getElementById("todoTitle");
-    addTodo(foo);
-    showTodo();
+    let foo = document.getElementById("todoTitle").value;
+    if (foo.length > 0){
+        addTodo(foo);
+        placeForTodos.innerHTML = null;
+        showTodos();
+        document.getElementById("registerTodo").reset();
+        if (!bool){
+            addTodoOptions();
+        }
+    }
 }
 
 function showStatus(todoStatus){
-    let foo = statusText 
+    let statusText = String;
+
     if (todoStatus){
-        foo = 'Competed';
+        statusText = 'Competed';
     } else {
-        foo = 'Pending';
+        statusText = 'Pending';
     }
-    return foo;
+    return statusText;
 }
 
 function showTodos(){
-    showTodo.innerHTML = null;
+    let placeForTodos = document.getElementById("placeForTodos");
+    placeForTodos.innerHTML = null;
     for (let i = 0; i < todos.length; i++) {
-        showTodo.innerHTML += "Id: " + todos[i].todoId + "Title: " + todos[i.todoTitle] + "Status: " + todos[i].todoStatus.showStatus;
-    }
-    divTasks.appendChild(displayedTodos);
+        placeForTodos.innerHTML += "Id: " + todos[i].todoId + ", Title: " + todos[i].todoTitle + ", Status: " + showStatus(todos[i].todoStatus) + "<br>";
+    };
 }
 
-function deleteTodo(){
-    let foo = document.getElementById('delete');
-    todos.splice((foo.todoId--), 1);
+function addTodoOptions(){
+    let foo = document.getElementById("showTodos");
+    let div = document.createElement("div");
+    div.id ="completed";
+    let h2 = document.createElement("h2");
+    h2.textContent = "Completed a task?";
+    let form = document.createElement("form");
+    form.id = "compeleTodo";
+    let label = document.createElement("label");
+    label.for = "Id";
+    label.textContent = "Choose Id of completed todo";
+    let input = document.createElement("input");
+    input.type = "number";
+    input.id = "completedId";
+    input.min = "1";
+    let button = document.createElement("button");
+    button.textContent = "Mark as done :)";
+    button.id = "btnCompletedTodo";
+    foo.parentNode.appendChild(div);
+    div.appendChild(h2);
+    h2.parentNode.appendChild(form);
+    form.appendChild(label);
+    form.appendChild(input);
+    form.parentNode.appendChild(button);
+    document.getElementById("btnCompletedTodo").addEventListener("click", completeTodo);
+    bool=true;
+}
+
+function completeTodo(){
+    if (bool){
+        let completedId = document.getElementById("completedId").value;
+        console.log(completedId);
+        if (completedId >= 1 && completedId <= todos.length){
+            todos[completedId-1].status = true;
+        }
+    }
+    document.getElementById("compeleTodo").reset();
+    showTodos()
 }
